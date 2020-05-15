472d1663c38cf7f69e9ba79e53f7bc6676395be2
Shane Holloway <shane.holloway@ieee.org>
2020-05-14 23:37:22 -0600

    Updated integration template

---
+++ test/int--tmpl/integ-test.mjs
@@ -0,0 +1,10 @@
+import validate_backend from '@phorbas/store/esm/node/validate_backend.mjs'
+
+/*
+import bkc_with_XYZ from '@phorbas/store/esm/XYZ.mjs'
+
+validate_backend('XYZ', ()=>
+  bkc_with_XYZ({
+    path: '/var/phorbas/test-XYZ/' }))
+*/
+
--- test/int--tmpl/rollup.config.js
+++ test/int--tmpl/rollup.config.js
@@ -1,5 +1,4 @@
 import {builtinModules} from 'module'
-import rpi_jsy from 'rollup-plugin-jsy'
 import rpi_dgnotify from 'rollup-plugin-dgnotify'
 import rpi_resolve from '@rollup/plugin-node-resolve'
 
@@ -8,13 +7,10 @@ const _cfg_ = {
   plugins: [
     rpi_dgnotify(),
     rpi_resolve(),
-    rpi_jsy(),
   ]}
 
-export default [
-  { ..._cfg_, input: `./integ-test.jsy`, output: [
-    { file: `esm/integ-test.mjs`, format: 'es', sourcemap: true },
-    { file: `cjs/integ-test.cjs`, format: 'cjs', exports: 'named', sourcemap: true },
-  ]}
-]
+export default { ..._cfg_,
+  input: `./integ-test.mjs`,
+  output: { file: `cjs/integ-test.cjs`, format: 'cjs', exports: 'named', sourcemap: true },
+}
 
