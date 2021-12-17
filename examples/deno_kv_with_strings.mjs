e803e8cc09900f47f72ab3ece9bae1049cbc0db0
Shane Holloway <shane.holloway@ieee.org>
2021-12-16 21:30:14 -0700

    Added deno examples

---
+++ examples/deno_kv_with_strings.mjs
@@ -0,0 +1,18 @@
+import {phorbas_kv_store, bkc_with_js_map} from 'https://cdn.jsdelivr.net/npm/@phorbas/store@0.2.14/esm/index.mjs'
+
+let stg = await phorbas_kv_store(bkc_with_js_map())
+
+let demo_body = 'hello phorbas kv store'
+console.log({demo_body})
+
+let u8_key = await stg.store_utf8(demo_body)
+console.log({u8_key})
+
+let rt_exists = await stg.exists(u8_key)
+console.log({rt_exists})
+
+let rt_utf8 = await stg.fetch_utf8(u8_key)
+console.log({rt_utf8})
+
+let rt_u8 = await stg.fetch_content(u8_key)
+console.log({rt_u8})
