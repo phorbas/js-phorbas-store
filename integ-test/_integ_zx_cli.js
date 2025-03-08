5c32035c649287c3dbb81abd30acef93d21b31e3
Shane Holloway <shane.holloway@ieee.org>
2025-03-08 15:32:40 -0700

    Updated keyv adapter and added to integ--local-filesystem

--- integ-test/_integ_zx_cli.js
+++ integ-test/_integ_zx_cli.js
@@ -3,8 +3,7 @@ import 'zx/globals'
 if (!$.quiet) $.verbose = true
 
 export const _integ_argv = process.argv.slice(
-  process.argv.findIndex(
-    sz => sz.match(/\.md$/)))
+  1 + process.argv.findIndex(sz => sz.match(/\.md$/)))
 
 export const integ_cli = minimist(_integ_argv)
 export {integ_cli as cli}
@@ -12,7 +11,7 @@ export {integ_cli as cli}
 export async function as_integ_config(...args) {
   let cfg = Object.assign({}, ...args)
 
-  cfg.rollup_files ||= ['integ-*-test.{mjs,jsy}']
+  cfg.rollup_files ||= ['*{.,-}test.{mjs,jsy}', 'test{.,-}*.{mjs,jsy}']
   cfg.rollup_files = await glob(... cfg.rollup_files)
 
   cfg.rollup_args ??= "--config=../rollup.integ-test.config.js"
--- integ-test/integ--local-filesystem/README.md
+++ integ-test/integ--local-filesystem/README.md
@@ -12,17 +12,13 @@ const cfg = await as_integ_config(pkg.integ_test)
 await fs.emptyDir(`./esm-test`)
 
 if (false !== cli.build) {
-  cfg.rollup_files = await glob(... cfg.rollup_files || ['integ-*-test.{mjs,jsy}'])
   await $`npx rollup ${cfg.rollup_args} ${cfg.rollup_files}`
-
-  $`ls ./esm-test`
 }
 
 if (false !== cli.docker) {
   await $`docker build ${cfg.docker_args} .`
   let _dkr_id_ = await $`docker build ${cfg.docker_args} -q .`
-
-  await $`docker run --rm -t ${_dkr_id_}`
+  await $`docker run --rm -t ${_dkr_id_} ${cli._}`
 }
 ```
 
