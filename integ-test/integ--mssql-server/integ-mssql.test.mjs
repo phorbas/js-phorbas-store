b7222d07dc1cff7686f00628d4351852ef5e2e42
Shane Holloway <shane.holloway@ieee.org>
2025-04-12 13:01:56 -0600

    Updated MSSQL Server integration tests

---
+++ integ-test/integ--mssql-server/integ-mssql.test.mjs
@@ -0,0 +1,50 @@
+import * as test_bdd from 'node:test'
+
+import {validate_backend} from '@phorbas/store/esm/validate_backend.js'
+import {bkc_with_knex} from '@phorbas/store/esm/sql/knex.js'
+import knex from 'knex'
+
+// List MSSQL versions to test -- see ./deps-deploy.yml
+// 'mssql' client uses 'tedious' dependency
+const all_mssql_clients = ['mssql']
+const mssql_hosts = {
+    'mssql_latest': all_mssql_clients,
+    'mssql_2022': all_mssql_clients,
+    'mssql_2019': all_mssql_clients,
+    'mssql_2017': all_mssql_clients,
+  }
+
+for (const [host, valid_client_list] of Object.entries(mssql_hosts)) {
+  for (const client of valid_client_list) {
+    validate_backend(test_bdd,
+      `knex to ${host}: ${client} client`,
+      { 
+        async bkc_create(ctx) {
+          ctx.kdb = knex({ client,
+            connection: {
+              host, user: 'sa',
+              password: 'int3g_Pass',
+            }})
+          return bkc_with_knex( ctx.kdb )
+        },
+
+        bkc_cleanup: ctx => ctx.kdb.destroy(),
+      })
+
+    validate_backend(test_bdd,
+      `knex to ${host}, immutable: ${client} client`,
+      { 
+        async bkc_create(ctx) {
+          ctx.kdb = knex({ client,
+            connection: {
+              host, user: 'sa',
+              password: 'int3g_Pass',
+            }})
+          return bkc_with_knex( ctx.kdb, { immutable: true })
+        },
+
+        bkc_cleanup: ctx => ctx.kdb.destroy(),
+      })
+  }
+}
+
